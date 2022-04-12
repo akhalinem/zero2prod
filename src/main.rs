1d696557b82acae87844d4c98cd6874754d285ad
Abror Xalilov <a.xalilov1997@gmail.com>
2022-04-12 23:57:01 +0500

    rearrange files for integration test' clear

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,6 @@
-use actix_web::{web, App, HttpResponse, HttpServer};
-
-async fn health_check() -> HttpResponse {
-    HttpResponse::Ok().into()
-}
+use zero2prod::run;
 
 #[actix_web::main] // or #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| App::new().route("/health_check", web::get().to(health_check)))
-        .bind(("127.0.0.1", 8080))?
-        .run()
-        .await
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[actix_web::test]
-    async fn health_check_succeeds() {
-        let res = health_check().await;
-        assert!(res.status().is_success())
-    }
+    run().await
 }
