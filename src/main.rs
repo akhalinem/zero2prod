d47e31247574edf285fa1943e64cf13ec6051a64
Abror Xalilov <a.xalilov1997@gmail.com>
2022-04-13 05:01:26 +0500

    make address dynamic

--- src/main.rs
+++ src/main.rs
@@ -2,5 +2,5 @@ use zero2prod::run;
 
 #[actix_web::main] // or #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    run()?.await
+    run("127.0.0.1:8000")?.await
 }
--- tests/health_check.rs
+++ tests/health_check.rs
@@ -7,7 +7,7 @@ async fn health_check_works() {
     let client = reqwest::Client::new();
 
     let response = client
-        .get("http://127.0.0.1:8080/health_check")
+        .get("http://127.0.0.1:8000/health_check")
         .send()
         .await
         .expect("Failed to execute request");
@@ -17,7 +17,7 @@ async fn health_check_works() {
 }
 
 fn spawn_app() {
-    let server = zero2prod::run().expect("Failed to bind adress");
+    let server = zero2prod::run("127.0.0.1:8000").expect("Failed to bind adress");
 
     let _ = tokio::spawn(server);
 }
