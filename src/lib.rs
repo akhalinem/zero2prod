a3d6f7caef5b08936ddf92860200c999883898e1
Abror Xalilov <a.xalilov1997@gmail.com>
2022-04-13 22:17:11 +0500

    implement basic subscribe endpoint

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,22 @@
-use std::net::TcpListener;
-
 use actix_web::{dev::Server, web, App, HttpResponse, HttpServer};
+use std::net::TcpListener;
 
 async fn health_check() -> HttpResponse {
     HttpResponse::Ok().finish()
 }
 
+async fn subscribe() -> HttpResponse {
+    HttpResponse::Ok().finish()
+}
+
 pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
-    let server = HttpServer::new(|| App::new().route("/health_check", web::get().to(health_check)))
-        .listen(listener)?
-        .run();
+    let server = HttpServer::new(|| {
+        App::new()
+            .route("/health_check", web::get().to(health_check))
+            .route("/subscriptions", web::post().to(subscribe))
+    })
+    .listen(listener)?
+    .run();
 
     Ok(server)
 }
