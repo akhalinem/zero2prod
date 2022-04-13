6743317a43c9ca10b789a71b78ed75dd1b50efad
Abror Xalilov <a.xalilov1997@gmail.com>
2022-04-13 05:18:19 +0500

    use random port only for test runtime

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ use zero2prod::run;
 
 #[actix_web::main] // or #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
+    let listener = TcpListener::bind("127.0.0.1:8000").expect("Failed to bind random port");
 
     run(listener)?.await
 }
