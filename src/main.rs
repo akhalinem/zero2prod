c0a067550ace98c3015ad812cf4cbde3aec3775f
Abror Xalilov <a.xalilov1997@gmail.com>
2022-04-16 01:31:00 +0500

    introduce submodules

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,8 @@
 use std::net::TcpListener;
 
-use zero2prod::run;
-
 #[actix_web::main] // or #[tokio::main]
 async fn main() -> std::io::Result<()> {
     let listener = TcpListener::bind("127.0.0.1:8000").expect("Failed to bind random port");
 
-    run(listener)?.await
+    zero2prod::startup::run(listener)?.await
 }
