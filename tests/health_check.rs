fb2db1829ec409d26a86a386ff62979c6e3944bb
Abror Xalilov <a.xalilov1997@gmail.com>
2022-04-18 21:21:57 +0500

    assert if subscription is saved to db

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -36,7 +36,7 @@ async fn subscribe_returns_a_200_for_valid_form_data() {
     let connection_string = configuration.database.connection_string();
     // The `Connection` trait MUST be in scope for us to invoke
     // `PgConnection::connect` - it is not an inherent method od the struct!
-    let connection = PgConnection::connect(&connection_string)
+    let mut connection = PgConnection::connect(&connection_string)
         .await
         .expect("Failed to connect to Postgres");
     let client = reqwest::Client::new();
@@ -51,6 +51,14 @@ async fn subscribe_returns_a_200_for_valid_form_data() {
         .expect("Failed to execute request");
 
     assert_eq!(200, response.status().as_u16());
+
+    let saved = sqlx::query!("SELECT email, name FROM subscriptions")
+        .fetch_one(&mut connection)
+        .await
+        .expect("Failed to fetch saved subscription");
+
+    assert_eq!(saved.email, "a.xalilov1997@gmail.com");
+    assert_eq!(saved.name, "Abror");
 }
 
 #[actix_rt::test]
