1a59ed3edcafdfb8542f170ab194ee24e19b152f
Abror Xalilov <a.xalilov1997@gmail.com>
2022-04-21 21:57:48 +0500

    implement db pooling

--- src/startup.rs
+++ src/startup.rs
@@ -1,19 +1,19 @@
 use actix_web::{dev::Server, web, App, HttpServer};
-use sqlx::PgConnection;
+use sqlx::PgPool;
 use std::net::TcpListener;
 
 use crate::routes::{health_check, subscribe};
 
-pub fn run(listener: TcpListener, connection: PgConnection) -> Result<Server, std::io::Error> {
-    // Wrap the connection in a smart pointer
-    let connection = web::Data::new(connection);
+pub fn run(listener: TcpListener, db_pool: PgPool) -> Result<Server, std::io::Error> {
+    // Wrap the pool using web::Data, which boils down to an Arc smart pointer
+    let db_pool = web::Data::new(db_pool);
     // Capture `connection` from the surrounding environment
     let server = HttpServer::new(move || {
         App::new()
             .route("/health_check", web::get().to(health_check))
             .route("/subscriptions", web::post().to(subscribe))
             //  Register the connection as part of the application state
-            .app_data(connection.clone())
+            .app_data(db_pool.clone())
     })
     .listen(listener)?
     .run();
