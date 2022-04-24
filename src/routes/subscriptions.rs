1b0926aacbb2b1c4acbf3e080b3633aa1b36e62f
Abror Xalilov <a.xalilov1997@gmail.com>
2022-04-24 12:58:01 +0500

    correlate logs

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -11,12 +11,17 @@ pub struct FormData {
 }
 
 pub async fn subscribe(form: web::Form<FormData>, pool: web::Data<PgPool>) -> HttpResponse {
+    let request_id = Uuid::new_v4();
     log::info!(
-        "Adding '{}' '{}' as a new subscriber",
+        "request_id {} - Adding '{}' '{}' as a new subscriber",
+        request_id,
         form.email,
         form.name
     );
-    log::info!("Saving new subscriber details in the databse");
+    log::info!(
+        "request_id {} - Saving new subscriber details in the database",
+        request_id
+    );
     let result = sqlx::query!(
         r#"
         INSERT INTO subscriptions (id, email, name, subscribed_at)
@@ -32,11 +37,18 @@ pub async fn subscribe(form: web::Form<FormData>, pool: web::Data<PgPool>) -> Ht
 
     match result {
         Ok(_) => {
-            log::info!("New subscriber details have been saved");
+            log::info!(
+                "request_id {} - New subscriber details have been saved",
+                request_id
+            );
             HttpResponse::Ok().finish()
         }
         Err(e) => {
-            log::error!("Failed to execute query: {:?}", e);
+            log::error!(
+                "request_id {} - Failed to execute query: {:?}",
+                request_id,
+                e
+            );
             HttpResponse::InternalServerError().finish()
         }
     }
