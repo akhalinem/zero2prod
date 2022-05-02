951b3cdb50fc71bc3f6481953286b891c56cdd38
Abror Xalilov <a.xalilov1997@gmail.com>
2022-05-02 13:20:42 +0500

    implement name validation

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -3,6 +3,7 @@ use chrono::Utc;
 use serde::Deserialize;
 use sqlx;
 use sqlx::PgPool;
+use unicode_segmentation::UnicodeSegmentation;
 use uuid::Uuid;
 
 #[derive(Deserialize)]
@@ -19,7 +20,12 @@ pub struct FormData {
         subscriber_name = %form.name
     )
 )]
+
 pub async fn subscribe(form: web::Form<FormData>, pool: web::Data<PgPool>) -> HttpResponse {
+    if !is_valid_name(&form.name) {
+        return HttpResponse::BadRequest().finish();
+    }
+
     let result = insert_subscriber(&pool, &form).await;
 
     match result {
@@ -46,4 +52,24 @@ pub async fn insert_subscriber(pool: &PgPool, form: &FormData) -> Result<(), sql
         e
     })?;
     Ok(())
+}
+
+/// on subscriber names, `false` otherwise
+pub fn is_valid_name(s: &str) -> bool {
+    let is_empty_or_whitespace = s.trim().is_empty();
+
+    // A grapheme is defined by the Unicode standard as a "user-perceived"
+    // character: `å` is a single grapheme, but it is composed of two characters
+    // (`a` and ``).
+    //
+    // `graphemes` returns and iterator over the graphemes in the input `s`.
+    // `true` specifies what we want to use the extended grapheme definition set,
+    // the recommended one.
+    let is_too_long = s.graphemes(true).count() > 256;
+
+    let forbidden_characters = ['/', '(', ')', '"', '<', '>', '\\', '{', '}'];
+    let contains_forbidden_characters = s.chars().any(|g| forbidden_characters.contains(&g));
+
+    // Return `false` if any of our conditions have been violated
+    !(is_empty_or_whitespace || is_too_long || contains_forbidden_characters)
 }
--- tests/health_check.rs
+++ tests/health_check.rs
@@ -140,3 +140,31 @@ async fn subscribe_returns_a_400_when_data_is_missing() {
         );
     }
 }
+
+#[actix_rt::test]
+async fn subscribe_returns_a_200_when_fields_are_present_but_empty() {
+    let app = spawn_app().await;
+    let client = reqwest::Client::new();
+    let test_cases = vec![
+        ("name=&email=a.xalilov1997%40gmail.com", "empty name"),
+        ("name=Abror&email=", "empty email"),
+        ("name=Abror&email=definitely-not-an-email", "invalid email"),
+    ];
+
+    for (body, description) in test_cases {
+        let response = client
+            .post(&format!("{}/subscriptions", &app.address))
+            .header("Content-Type", "application/x-www-form-urlencoded")
+            .body(body)
+            .send()
+            .await
+            .expect("Failed to execute request");
+
+        assert_eq!(
+            200,
+            response.status().as_u16(),
+            "The API did not return a 200 OK when the payload was {}",
+            description
+        )
+    }
+}
